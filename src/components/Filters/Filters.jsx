3a3fb0d3a036e6290c09a681e1ee3033e17740f8
myusername <Matanfriedleet@hotmail.com>
2020-09-03 21:07:04 +0300

    added new branch

--- src/components/Filters/Filters.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact'
-import { MDBTypography } from 'mdbreact'
-import FilterCategories from './FilterCategories'
-const Filters = () => {
-  return <FilterCategories />
-}
-
-export default Filters
--- src/stores/Category.js
+++
@@ -1,8 +0,0 @@
-import { observable, action, computed } from 'mobx'
-
-export class Category{
-    @observable category 
-    constructor(category){
-        this.category = category
-    }
-}
